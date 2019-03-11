346d5c1f1f19eab6e909b0db1c9cd415839a5caa
jinwoo park <swat018@gmail.com>
2019-03-11 12:06:57 +0900

    headfirst servlet&JSP chapter3

---
+++ headfirst/Myprojects/beerV1/src/com/example/web/BeerSelect.java
@@ -0,0 +1,28 @@
+package com.example.web;
+
+import com.example.model.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+
+public class BeerSelect extends HttpServlet {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String c = request.getParameter("color");
+        BeerExpert be = new BeerExpert();
+        List result = be.getBrands(c);
+
+        // response.setContentType("text/html");
+        // PrintWriter out = response.getWriter();
+        // out.println("Beer Selection Advice<br>");
+
+        // Iterator it = result.iterator();
+        // while (it.hasNext()) {
+        // out.println("<br>try: " + it.next());
+        // }
+
+        request.setAttribute("styles", result);
+        RequestDispatcher view = request.getRequestDispatcher("result.jsp");
+        view.forward(request, response);
+    }
+}
